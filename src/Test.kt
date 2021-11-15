b607a569c6da2cfdc3362efdf9837a3c248842c0
ShaharMS <shaharmsecond@gmail.com>
2021-11-15 23:16:30 +0200

    starting my first project

---
+++ src/Test.kt
@@ -0,0 +1,35 @@
+import kotlin.random.Random
+
+class Game {
+
+    var num:Int = 0
+    var iterations:Int = 0;
+    fun generateNumber():Game {
+        num = Random.nextInt(0, 1000000); return this
+    }
+
+    fun start() {
+        val number = readLine()!!.toInt()
+
+        if (number > num) {
+            println("Lower!")
+            iterations++
+            start()
+        }
+        else if (number < num) {
+            println("Higher!")
+            iterations++
+            start()
+        }
+        else if (number == num) {
+            println("Nice One! You Hit The Secret Number In $iterations tries! Press ENTER To Try Again")
+            val sus = readLine()
+            main()
+        }
+    }
+}
+
+fun main() {
+    println("The Ultimate Game - Higher Or Lower!")
+    Game().generateNumber().start()
+}
